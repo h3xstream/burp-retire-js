103c473c1d5d7c450b30629617546fbab8724f81
Philippe Arteau <parteau@gosecure.ca>
2019-12-10 12:36:53 -0500

    Javadoc fixes avoid warning on release

--- retirejs-core/src/main/java/com/h3xstream/retirejs/repo/dl/Downloader.java
+++ retirejs-core/src/main/java/com/h3xstream/retirejs/repo/dl/Downloader.java
@@ -7,9 +7,15 @@ import java.net.MalformedURLException;
 public interface Downloader {
 
     /**
+     * Download the content at the URL given and save it locally.
+     * This interface is used to switch between HTTP connector.
+     *
+     * In the context of Maven, the connection need to pass through Maven API in order
+     * to benefit from its proxy configuration. The Maven plugin would otherwise not work on enterprise network.
+     *
      * @param url URL to request
      * @param file File where the content will be saved.
-     * @throws Exception
+     * @throws Exception Connection error most likely
      */
     void downloadUrlToFile(String url, File file) throws Exception;
 }
